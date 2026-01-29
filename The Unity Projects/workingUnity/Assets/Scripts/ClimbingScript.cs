1819fba77224e8b59d5bf142e1ab6da34be547d5
Allen Larsen <allenlarsen444@gmail.com>
2026-01-28 21:15:06 -0700

    Revert "Please work!"
    This reverts commit afc6faf737f80240b3c9d751ebc7cd06ea2605df.

--- The Unity Projects/workingUnity/Assets/Scripts/ClimbingScript.cs
+++ The Unity Projects/workingUnity/Assets/Scripts/ClimbingScript.cs
@@ -28,7 +28,11 @@ public class ClimbingScript : MonoBehaviour
         if (controller == null) return;
 
         // Enter climbing (K key)
+<<<<<<< HEAD
         if (!isClimbing && Input.GetKeyDown(KeyCode.K))
+=======
+        if (!isClimbing && Input.GetMouseButton(0))
+>>>>>>> parent of afc6faf (Please work!)
         {
             TryEnterClimb();
         }
@@ -80,7 +84,11 @@ public class ClimbingScript : MonoBehaviour
     private void HandleClimbing()
     {
         // J key to exit
+<<<<<<< HEAD
         if (Input.GetKeyDown(KeyCode.J))
+=======
+        if (Input.GetKeyDown(KeyCode.Space))
+>>>>>>> parent of afc6faf (Please work!)
         {
             ExitClimb();
             return;
